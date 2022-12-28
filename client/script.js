192a64c457cd60c6bf547680650843028a0ccd0d
shadowasphodel2919 <t2905j@gmail.com>
2022-12-28 22:13:57 +0530

    Added change

--- client/script.js
+++ client/script.js
@@ -69,7 +69,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://codinguru.onrender.com/',{
     method: 'POST',
         headers: {
             'Content-Type': 'application/json',
